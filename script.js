361fba1a76442d812e9f19add8ba3730c0ec2003
Top4eeG <A228_M1337@mail.ru>
2023-06-21 13:09:10 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
+
+$(window).on('load', function() {
+	$('.preloader').fadeOut().end().delay(400).fadeOut('slow');
+});
